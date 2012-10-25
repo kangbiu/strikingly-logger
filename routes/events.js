604845e3e14ced17abcc96771c0ba0d42d20aee8
Dafeng Guo <dfguo.joe@gmail.com>
2012-10-25 00:08:00 -0700

    use atomic calls

--- routes/events.js
+++ routes/events.js
@@ -4,27 +4,15 @@ var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
-var util = require("util");
-
 exports.addEvent = function(req, res) {
   var id = req.body.user_id
-  var event = req.body.event
+  var event = req.body.event.split(' ').join('_')
   console.log("Adding event: " + id + " " + event)
-  client.get('User.' + id, function (err, reply) {    
-    result = {'events':{}};
-    if (reply !== null) {
-      result = JSON.parse(reply);
-    }
-    
-    result['events'][event] = result['events'][event] || {'count': 0, 'timestamps':[]}; // initialize
-    resultEvent = result['events'][event];
-    
-    resultEvent['count']  += 1; // update the coun
+  
+  client.hincrby('user:'+id+':count', event, 1, function(e, reply) {
     var date = new Date();
     var ts = Math.round(date.getTime() / 1000);
-    resultEvent['timestamps'].push(ts); // insert new timestamp
-    
-    client.set('User.' + id, JSON.stringify(result));
-    res.send({count:resultEvent['count']});
+    client.lpush('user:'+id+':'+event, ts);
+    res.send({count:reply});
   });
 };
